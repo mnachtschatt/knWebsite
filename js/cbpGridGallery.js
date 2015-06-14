f8c365f22d73264fa5dd52f56d9cbc9d3703f53b
Stefan Nathaniel <e0727355@student.tuwien.ac.at>
2015-06-14 23:37:15 +0200

    renerenzen pop up

--- js/cbpGridGallery.js
+++ js/cbpGridGallery.js
@@ -98,78 +98,15 @@
 	CBPGridGallery.prototype._initEvents = function() {
 		var self = this;
 
-		// open the slideshow when clicking on the main grid items
-		this.gridItems.forEach( function( item, idx ) {
-			item.addEventListener( 'click', function() {
-				self._openSlideshow( idx );
-			} );
-		} );
-
-		// slideshow controls
-		this.ctrlPrev.addEventListener( 'click', function() { self._navigate( 'prev' ); } );
-		this.ctrlNext.addEventListener( 'click', function() { self._navigate( 'next' ); } );
-		this.ctrlClose.addEventListener( 'click', function() { self._closeSlideshow(); } );
+	
 
 		// window resize
 		window.addEventListener( 'resize', function() { self._resizeHandler(); } );
 
-		// keyboard navigation events
-		document.addEventListener( 'keydown', function( ev ) {
-			if ( self.isSlideshowVisible ) {
-				var keyCode = ev.keyCode || ev.which;
-
-				switch (keyCode) {
-					case 37:
-						self._navigate( 'prev' );
-						break;
-					case 39:
-						self._navigate( 'next' );
-						break;
-					case 27:
-						self._closeSlideshow();
-						break;
-				}
-			}
-		} );
-
-		// trick to prevent scrolling when slideshow is visible
-		window.addEventListener( 'scroll', function() {
-			if ( self.isSlideshowVisible ) {
-				window.scrollTo( self.scrollPosition ? self.scrollPosition.x : 0, self.scrollPosition ? self.scrollPosition.y : 0 );
-			}
-			else {
-				self.scrollPosition = { x : window.pageXOffset || docElem.scrollLeft, y : window.pageYOffset || docElem.scrollTop };
-			}
-		});
 	};
 
 	CBPGridGallery.prototype._openSlideshow = function( pos ) {
-		this.isSlideshowVisible = true;
-		this.current = pos;
 
-		classie.addClass( this.el, 'slideshow-open' );
-
-		/* position slideshow items */
-
-		// set viewport items (current, next and previous)
-		this._setViewportItems();
-		
-		// add class "current" and "show" to currentItem
-		classie.addClass( this.currentItem, 'current' );
-		classie.addClass( this.currentItem, 'show' );
-
-		// add class show to next and previous items
-		// position previous item on the left side and the next item on the right side
-		if( this.prevItem ) {
-			classie.addClass( this.prevItem, 'show' );
-			var translateVal = Number( -1 * ( getViewportW() / 2 + this.prevItem.offsetWidth / 2 ) );
-			setTransform( this.prevItem, support.support3d ? 'translate3d(' + translateVal + 'px, 0, -150px)' : 'translate(' + translateVal + 'px)' );
-		}
-		if( this.nextItem ) {
-			classie.addClass( this.nextItem, 'show' );
-			var translateVal = Number( getViewportW() / 2 + this.nextItem.offsetWidth / 2 );
-			setTransform( this.nextItem, support.support3d ? 'translate3d(' + translateVal + 'px, 0, -150px)' : 'translate(' + translateVal + 'px)' );
-		}
 	};
 
 	CBPGridGallery.prototype._navigate = function( dir ) {
@@ -212,121 +149,16 @@
 			classie.addClass( incomingItem, 'show' );
 		}
 
-		var slide = function() {
-			// add class animatable to the slideshow grid
-			classie.addClass( self.slideshow, 'animatable' );
+	
 
-			// overlays:
-			classie.removeClass( self.currentItem, 'current' );
-			var nextCurrent = dir === 'next' ? self.nextItem : self.prevItem;
-			classie.addClass( nextCurrent, 'current' );
-
-			setTransform( self.currentItem, dir === 'next' ? transformLeftStr : transformRightStr );
-
-			if( self.nextItem ) {
-				setTransform( self.nextItem, dir === 'next' ? transformCenterStr : transformOutStr );
-			}
-
-			if( self.prevItem ) {
-				setTransform( self.prevItem, dir === 'next' ? transformOutStr : transformCenterStr );
-			}
-
-			if( incomingItem ) {
-				setTransform( incomingItem, dir === 'next' ? transformRightStr : transformLeftStr );
-			}
-
-			var onEndTransitionFn = function( ev ) {
-				if( support.transitions ) {
-					if( ev.propertyName.indexOf( 'transform' ) === -1 ) return false;
-					this.removeEventListener( transEndEventName, onEndTransitionFn );
-				}
-
-				if( self.prevItem && dir === 'next' ) {
-					classie.removeClass( self.prevItem, 'show' );
-				}
-				else if( self.nextItem && dir === 'prev' ) {
-					classie.removeClass( self.nextItem, 'show' );
-				}
-
-				if( dir === 'next' ) {
-					self.prevItem = self.currentItem;
-					self.currentItem = self.nextItem;
-					if( incomingItem ) {
-						self.nextItem = incomingItem;
-					}
-				}
-				else {
-					self.nextItem = self.currentItem;
-					self.currentItem = self.prevItem;
-					if( incomingItem ) {
-						self.prevItem = incomingItem;
-					}
-				}
-
-				self.current = dir === 'next' ? self.current + 1 : self.current - 1;
-				self.isAnimating = false;
-			};
-
-			if( support.transitions ) {
-				self.currentItem.addEventListener( transEndEventName, onEndTransitionFn );
-			}
-			else {
-				onEndTransitionFn();
-			}
-		};
-
-		setTimeout( slide, 25 );
 	}
 
 	CBPGridGallery.prototype._closeSlideshow = function( pos ) {
-		// remove class slideshow-open from the grid gallery elem
-		classie.removeClass( this.el, 'slideshow-open' );
-		// remove class animatable from the slideshow grid
-		classie.removeClass( this.slideshow, 'animatable' );
 
-		var self = this,
-			onEndTransitionFn = function( ev ) {
-				if( support.transitions ) {
-					if( ev.target.tagName.toLowerCase() !== 'ul' ) return;
-					this.removeEventListener( transEndEventName, onEndTransitionFn );
-				}
-				// remove classes show and current from the slideshow items
-				classie.removeClass( self.currentItem, 'current' );
-				classie.removeClass( self.currentItem, 'show' );
-				
-				if( self.prevItem ) {
-					classie.removeClass( self.prevItem, 'show' );
-				}
-				if( self.nextItem ) {
-					classie.removeClass( self.nextItem, 'show' );
-				}
-
-				// also reset any transforms for all the items
-				self.slideshowItems.forEach( function( item ) { setTransform( item, '' ); } );
-
-				self.isSlideshowVisible = false;
-			};
-
-		if( support.transitions ) {
-			this.el.addEventListener( transEndEventName, onEndTransitionFn );
-		}
-		else {
-			onEndTransitionFn();
-		}
 	};
 
 	CBPGridGallery.prototype._setViewportItems = function() {
-		this.currentItem = null;
-		this.prevItem = null;
-		this.nextItem = null;
 
-		if( this.current > 0 ) {
-			this.prevItem = this.slideshowItems[ this.current - 1 ];
-		}
-		if( this.current < this.itemsCount - 1 ) {
-			this.nextItem = this.slideshowItems[ this.current + 1 ];
-		}
-		this.currentItem = this.slideshowItems[ this.current ];
 	}
 
 	// taken from https://github.com/desandro/vanilla-masonry/blob/master/masonry.js by David DeSandro
